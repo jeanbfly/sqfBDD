f4d85a9abe9964863ce3fb6d9bf902dec93505e7
jeanbfly <jean.bourgies@gmail.com>
2022-11-26 16:27:14 +0100

    suppression fichiers inutiles

--- createTable.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('bdd.db')
-c = conn.cursor()
-
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
-
-conn.close()
